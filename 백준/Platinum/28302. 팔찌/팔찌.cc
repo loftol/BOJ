7d55630c803ade8ae3f494eed6c829d209ee63d8
loftol <60003939+loftol@users.noreply.github.com>
2023-07-07 00:59:13 +0900

    [Platinum I] Title: 팔찌, Time: 0 ms, Memory: 2296 KB -BaekjoonHub

--- 백준/Platinum/28302. 팔찌/팔찌.cc
+++ 백준/Platinum/28302. 팔찌/팔찌.cc
@@ -1,164 +1,103 @@
-#include <iostream>
-#include <vector>
-#include <tuple>
-#include <deque>
+#include <bits/stdc++.h>
 #include <array>
-#include <string>
-
+#define ll long long int
+#define ld long double
+#define tii array<ld, 3>
+#define pld pair<ld, ld>
+#define all(v) v.begin(),v.end()
 using namespace std;
-
-vector<int> str, str1, str2;
-
-vector<string> queries, queries1, queries2;
-
-char revHash(int X) {
-	string st = " RGB";
-	return st[X];
-}
-
-string format1(int a, int b, int c) {
-	return to_string(a) + ' ' + to_string(b) + ' ' + to_string(c);
-}
-
-string format2(int a, int b, char c, char d) {
-	return to_string(a) + ' ' + to_string(b) + ' ' + c + ' ' + d;
-}
-
-void SWAP(int a, int X, int Y) {
-	queries.push_back(format1(1, a, a + 1));
-	queries.push_back(format2(2, a, revHash(Y), revHash(X)));
-	swap(str[a - 1], str[a]);
-}
-
-int XY(int a, int X, int Y) {
-	queries.push_back(format1(1, a, a + 1));
-	str.pop_back(), str.pop_back();
-	str.push_back(X ^ Y);
-	return (X ^ Y);
-}
-
-void XYY(int a, int X, int Y) {
-	int Z = X ^ Y;
-	queries.push_back(format1(1, a, a + 1));
-	queries.push_back(format1(1, a, a + 1));
-	str.pop_back(), str.pop_back();
-}
-
-void XXX(int a, int X) {
-	int Y = (X == 1) ? 2 : 1;
-	int Z = X ^ Y;
-	queries.push_back(format2(2, a + 1, revHash(Y), revHash(Z)));
-	queries.push_back(format1(1, a + 2, a + 3));
-	XYY(a, X, Y);
-}
-
-void XX2YY(int a, int X, int Y) {
-	int Z = X ^ Y;
-	queries1.push_back(format2(2, a + 1, revHash(Z), revHash(Y)));
-	queries1.push_back(format1(1, a, a + 1));
-	str[a - 1] = Y, str[a] = Y;
-}
-
-void revSWAP(int a, int X, int Y) {
-	SWAP(a, X, Y);
-}
-
-int revXY(int a, int X, int Y) {
-	int Z = X ^ Y;
-	queries.push_back(format2(2, a, revHash(X), revHash(Y)));
-	str.pop_back(), str.pop_back();
-	str.push_back(Z);
-	return Z;
-}
-
-void revXXX(int a, int X) {
-	int Y = (X == 1) ? 2 : 1;
-	int Z = X ^ Y;
-	queries.push_back(format1(1, a + 2, a + 3));
-	queries.push_back(format2(2, a + 1, revHash(X), revHash(Y)));
-	queries.push_back(format2(2, a, revHash(X), revHash(Z)));
-	queries.push_back(format2(2, a, revHash(Y), revHash(Z)));
-	str.pop_back(), str.pop_back();
-}
-
-void revXYY(int a, int X, int Y) {
-	int Z = X ^ Y;
-	queries.push_back(format2(2, a, revHash(X), revHash(Y)));
-	queries.push_back(format2(2, a, revHash(Z), revHash(Y)));
-	str.pop_back(), str.pop_back();
-}
 
 int hashf(char c) {
 	if (c == 'R') return 1;
-	if (c == 'G') return 2;
-	if (c == 'B') return 3;
+	else if (c == 'G') return 2;
+	else if (c == 'B') return 3;
+	else return 0;
 }
 
-void subSolve1() {
-	while (!((str.size() == 2 && (str[0] == str[1])) || str.size() == 1)) {
-		int e = str.size() - 1;
-		if (str[e] != str[e - 1]) XY(e, str[e - 1], str[e]);
+string revHash = " RGB";
+
+string format1(int i, int j) {
+	return "1 " + to_string(i) + ' ' + to_string(j);
+}
+
+string format2(int i, char a, char b) {
+	return "2 " + to_string(i) + ' ' + a + ' ' + b;
+}
+
+void solve()
+{
+	int n1, n2;
+	string str1, str2;
+	cin >> n1 >> str1 >> n2 >> str2;
+	int xor1 = 0;
+	for (char c : str1) xor1 ^= hashf(c);
+	for (char c : str2) xor1 ^= hashf(c);
+	if (xor1) {
+		cout << -1; return;
+	}
+
+	int idx1 = 0, idx2 = 0;
+	vector<string> Q;
+	for (; idx2 < n2; idx2++) {
+		if (str1[idx1] == str2[idx2]) {
+			if (idx1 == str1.size() - 1 && idx2 != str2.size()) {
+				char x = str1[idx1];
+				char y = (x == 'R') ? 'G' : 'R';
+				char z = revHash[hashf(x) ^ hashf(y)];
+				Q.push_back(format2(idx2 + 1, y, z));
+				Q.push_back(format2(idx2 + 1, x, z));
+				str1.push_back(z); str1.push_back(z);
+			}
+			idx1++;
+		}
 		else {
-			if (str[e - 1] != str[e - 2]) XYY(e - 1, str[e - 2], str[e - 1]);
-			else XXX(e - 1, str[e - 2]);
+			char a = str2[idx2];
+			str1[idx1] = revHash[hashf(str1[idx1]) ^ hashf(str2[idx2])];
+			char b = str1[idx1];
+			Q.push_back(format2(idx2 + 1, a, b));
 		}
 	}
-	str1 = str;
-	queries1 = queries;
-}
 
-void subSolve2() {
-	while (!((str.size() == 2 && (str[0] == str[1])) || str.size() == 1)) {
-		int e = str.size() - 1;
-		if (str[e] != str[e - 1]) revXY(e, str[e - 1], str[e]);
+	string str(str1.begin() + idx1, str1.end());
+	char c = str2.back();
+	if(str.size())
+		reverse(all(str));
+	int pivot = str2.size();
+
+	while (str.size()) {
+		int e = (int)str.size() - 1;
+		assert(str.size() != 1);
+		char x = str[e], y = str[e - 1];
+		if (x == y) {
+			if (x == c) {
+				char u = (x == 'R') ? 'G' : 'R';
+				char v = revHash[hashf(x) ^ hashf(u)];
+				Q.push_back(format2(pivot + 1, u, v));
+				Q.push_back(format1(pivot + 2, pivot + 3));
+			}
+			Q.push_back(format1(pivot, pivot + 1));
+			Q.push_back(format1(pivot, pivot + 1));
+			str.pop_back(); str.pop_back();
+		}
 		else {
-			if (str[e - 1] != str[e - 2]) revXYY(e - 1, str[e - 2], str[e - 1]);
-			else revXXX(e - 1, str[e - 2]);
+			char u = revHash[hashf(x) ^ hashf(y)];
+			Q.push_back(format1(pivot + 1, pivot + 2));
+			str.pop_back();
+			str[e - 1] = u;
 		}
 	}
-	str2 = str;
-	queries2 = vector<string>(queries.rbegin(), queries.rend());
+
+	cout << Q.size() << '\n';
+	for (string q : Q) {
+		cout << q << '\n';
+	}
+
 }
 
-void solve() {
-	int n;
-	string s;
-	cin >> n >> s;
-	
-	for (char c : s) str.push_back(hashf(c));
-
-	subSolve1();
-
-	cin >> n >> s;
-	str = vector<int>();
-	queries = vector<string>();
-	for (char c : s) str.push_back(hashf(c));
-	
-	subSolve2();
-
-	if (str1.size() != str2.size()) {
-		cout << -1;
-		return;
-	}
-
-	if (str1.size() == 1) {
-		if (str1[0] != str2[0]) {
-			cout << -1;
-			return;
-		}
-	}
-	else {
-		if (str1[0] != str2[0]) {
-			XX2YY(1, str1[0], str2[0]);
-		}
-	}
-	cout << queries1.size() + queries2.size() << '\n';
-	for (string query : queries1) cout << query << '\n';
-	for (string query : queries2) cout << query << '\n';
-}
-
-int main() {
-	ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
-	solve();
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int tc = 1; //cin >> tc;
+	while (tc--) solve();
 	return 0;
 }
