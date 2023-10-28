b6432b48942785a5fe425ce79cedab12dd177fe2
loftol <60003939+loftol@users.noreply.github.com>
2023-10-28 19:13:00 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 572 ms, Memory: 162524 KB -BaekjoonHub

---
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
@@ -0,0 +1,13 @@
+def f(n, fr, to):
+    if(n == 0): return
+    x = -1
+    arr = [fr, to]
+    if(1 not in arr) : x = 1
+    elif(2 not in arr) : x = 2
+    elif(3 not in arr) : x = 3
+    f(n - 1, fr, x)
+    print(fr, to)
+    f(n-1, x, to)
+n = int(input())
+print(2 ** n - 1)
+f(n, 1, 3)
