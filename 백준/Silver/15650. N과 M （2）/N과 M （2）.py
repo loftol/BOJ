cbe54f9b79f7b7c6ed1c72f3878f449bb44da5f1
loftol <60003939+loftol@users.noreply.github.com>
2023-11-04 01:35:05 +0900

    [Silver III] Title: N과 M (2), Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,15 +1,16 @@
 n, m = map(int, input().split())
 
-def f(depth, prev, arr):
+def f(depth, arr):
     global n, m
     if(depth == m):
-        for i in arr:
+        for i in arr: # 탐색 구조상 arr는 이미 항상 정렬되어 있습니다.(why?)
             print(i, end=" ")
         print()
         return
-    for i in range(prev + 1, n + 1):
+    prev = arr[-1] if len(arr) else 0
+    for i in range(prev + 1, n + 1): #
         arr.append(i)
-        f(depth + 1, i, arr)
+        f(depth + 1, arr)
         arr.pop()
 
-f(0, 0, [])
+f(0, [])
