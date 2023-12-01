1e426cee0826b4b8a53ca15398567d74efb7c738
loftol <60003939+loftol@users.noreply.github.com>
2023-12-02 02:12:01 +0900

    [Gold II] Title: 1의 개수 세기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9527. 1의 개수 세기/1의 개수 세기.cc
+++ 백준/Gold/9527. 1의 개수 세기/1의 개수 세기.cc
@@ -1,17 +1,11 @@
 #include<bits/stdc++.h>
-
 #define ll long long
 using namespace std;
-
 int main() {
     ll a, b; cin >> a >> b;
     auto cnt = [](ll a) {
-        ll x = 1;
-        ll ret = 0;
-        while (x <= a) {
-            x <<= 1;
-            ret += a / x * x / 2 + max((a % x) - x / 2 + 1, 0LL);
-        }
+        ll x = 1, ret = 0;
+        while (x <= a) x <<= 1,ret += a / x * x / 2 + max((a % x) - x / 2 + 1, 0LL);
         return ret;
     };
     cout << cnt(b) - cnt(a - 1);
