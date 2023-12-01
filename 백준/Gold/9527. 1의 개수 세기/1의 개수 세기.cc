fd2c244d88550bb96a0d8f2586b61c2e64a11bf2
loftol <60003939+loftol@users.noreply.github.com>
2023-12-02 02:10:08 +0900

    [Gold II] Title: 1의 개수 세기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9527. 1의 개수 세기/1의 개수 세기.cc
+++ 백준/Gold/9527. 1의 개수 세기/1의 개수 세기.cc
@@ -1,35 +1,18 @@
 #include<bits/stdc++.h>
-#include<array>
-using namespace std;
-int INF = 50000000;
-#define ll long long
-#define all(x) x.begin(), x.end()
-#define pii array<int, 2>
-#define tii array<int, 3>
 
-void solve() {
+#define ll long long
+using namespace std;
+
+int main() {
     ll a, b; cin >> a >> b;
     auto cnt = [](ll a) {
         ll x = 1;
         ll ret = 0;
         while (x <= a) {
             x <<= 1;
-            ll ub = (a / x * x + x);
-            ret += ub / 2;
-            ll sub = a % x;
-            sub = x / 2 - max(sub - x / 2 + 1, 0LL);
-            ret -= sub;
+            ret += a / x * x / 2 + max((a % x) - x / 2 + 1, 0LL);
         }
         return ret;
     };
     cout << cnt(b) - cnt(a - 1);
-}
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-    int t = 1; //cin >> t;
-    while (t--) {
-        solve();
-    }
 }
