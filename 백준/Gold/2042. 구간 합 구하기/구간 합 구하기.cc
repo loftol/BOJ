d2a05f1ab25f32287807c49c68e6088d20a10dbc
loftol <60003939+loftol@users.noreply.github.com>
2023-12-16 05:32:56 +0900

    [Gold I] Title: 구간 합 구하기, Time: 216 ms, Memory: 26224 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -1,76 +1,78 @@
-#include <vector>
-#include <iostream>
-#include <queue>
-#include <array>
-#include <set>
-#include <cassert>
-#include <algorithm>
-#include <cmath>
-#include <bit>
-
-#define ll long long
-#define vi vector<int>
-#define mat vector<vi>
-#define pii pair<int, int>
-#define tii array<int, 3>
-#define all(v) v.begin(),v.end()
+#include <bits/stdc++.h>
+#include<array>
 
 using namespace std;
 
-vector<ll> seg;
+#define pii array<int, 2>
+#define ll long long
+#define tll array<ll, 3>
 
-int n, S = 1, m, k;
+template<typename T>
+struct Segtree {
+	int S;
+	vector<T> seg;
+	T(*oper)(T, T);
+	T e;
 
-void init() {
-	S = bit_ceil((unsigned int)n);
-	seg = vector<ll>(2 * S);
-
-	for (int i = 0; i < n; i++)
-		cin >> seg[S + i];
-	
-	for (int SS = S / 2; SS > 0; SS /= 2)
-		for (int i = SS; i < 2 * SS; i++)
-			seg[i] = seg[2 * i] + seg[2 * i + 1];
-}
-
-void update(int idx, ll val) {
-	idx += S;
-	seg[idx] = val;
-	for (idx /= 2; idx > 0; idx /= 2) 
-		seg[idx] = seg[2 * idx] + seg[2 * idx + 1];
-}
-
-ll query(int l, int r) {
-	l += S;
-	r += S;
-	ll ret = 0;
-	while (l <= r) {
-		if (l & 1)
-			ret += seg[l++];
-		if (r & 1 ^ 1)
-			ret += seg[r--];
-		l >>= 1;
-		r >>= 1;
+	Segtree() {
+		S = 0;
+		seg = vector<T>(2 * S);
+		oper = [](T a, T b) {return a; };
+		e = T();
 	}
-	return ret;
+
+	Segtree(const vector<T>& arr, T _e = 0, T(*_oper)(T, T) = [](T a, T b) {return a + b; }) {
+		int _n = arr.size();
+		S = bit_ceil((unsigned int)_n);
+		seg = vector<T>(2 * S, _e);
+		oper = _oper;
+		e = _e;
+
+		for (int i = 0; i < _n; i++)
+			seg[S + i] = arr[i];
+
+		for (int SS = S / 2; SS > 0; SS /= 2)
+			for (int i = SS; i < 2 * SS; i++)
+				seg[i] = oper(seg[2 * i], seg[2 * i + 1]);
+	}
+
+	void update(int idx, T val) {
+		idx += S;
+		seg[idx] = val;
+		for (idx >>= 1; idx > 0; idx >>= 1)
+			seg[idx] = oper(seg[idx * 2], seg[idx * 2 + 1]);
+	}
+
+	T query(int l, int r) {
+		T lret = e, rret = e;
+		for (l += S, r += S; l <= r; l >>= 1, r >>= 1) {
+			if (l & 1) lret = oper(lret, seg[l++]);
+			if (r & 1 ^ 1) rret = oper(seg[r--], rret);
+		}
+		return oper(lret, rret);
+	}
+};
+
+void solve() {
+	int n, m, k; cin >> n >> m >> k;
+	vector<ll> arr(n); for (ll& i : arr) cin >> i;
+	Segtree<ll> seg(arr);
+
+	for (int i = 0; i < m + k; i++) {
+		int q; cin >> q;
+		ll x, y; cin >> x >> y;
+		if (q == 1) seg.update(x - 1, y);
+		else cout << seg.query(x - 1, y - 1) << '\n';
+	}
 }
 
 int main() {
-	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
-	cin >> n >> m >> k;
-	init();
-	m += k;
-	while (m--) {
-		ll a, b, c;
-		cin >> a >> b >> c;
-		if (a == 1) {
-			update(b - 1, c);
-		}
-		else {
-			cout << query(b - 1, c - 1) << '\n';
-		}
-	}
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+#endif
+	int t = 1; //cin >> t;
+	while (t--) solve();
 
 	return 0;
 }
