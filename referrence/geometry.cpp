73da501386bca5a96ff7dd1608835e01f5b0fdfc
loftol <60003939+loftol@users.noreply.github.com>
2023-12-19 00:52:50 +0900

    Update geometry.cpp

--- referrence/geometry.cpp
+++ referrence/geometry.cpp
@@ -14,6 +14,11 @@ auto EUC_DST = [&](pii a, pii b){
 	return sqrt(DOT(SUB(a, b), SUB(a, b)));
 };
 
+auto TAXI_DST = [&](pii a, pii b){
+	pii sub = SUB(a, b);
+	return abs(sub[0]) + abs(sub[1]);
+};
+
 auto CCW = [&](pii o, pii a, pii b) {
 	ll op = OP(SUB(a, o), SUB(b, o));
 	if (op) return op / abs(op);
