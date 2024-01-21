ab0263fb610802c4930177e1679a8544b7de8170
loftol <60003939+loftol@users.noreply.github.com>
2024-01-21 23:15:20 +0900

    [Silver III] Title: Comfortable Cows, Time: 212 ms, Memory: 118992 KB -BaekjoonHub

--- 백준/Silver/21236. Comfortable Cows/Comfortable Cows.py
+++ 백준/Silver/21236. Comfortable Cows/Comfortable Cows.py
@@ -1,25 +1,28 @@
-def solve():
-	cnt = [[-1] * 1001 for i in range(1001)]
-	dx = [0, 1, 0, -1]
-	dy = [1, 0, -1, 0]
-	n = int(input())
-	ans = 0
- 
-	for i in range(n):
-		x, y = map(int, input().split())
-		cnt[x][y] = 0
-		for dir in range(4):
-			nx = x + dx[dir]
-			ny = y + dy[dir]
-			if(nx >= 0 and nx <= 1000 and ny >= 0 and ny <= 1000):
-				if(cnt[nx][ny] >= 0):
-					cnt[nx][ny] += 1
-					cnt[x][y] += 1
-					if(cnt[nx][ny] == 3):
-						ans += 1
-					elif(cnt[nx][ny] == 4):
-						ans -= 1
-			if(cnt[x][y] == 3): ans += 1
-		print(ans)	
+import sys
+input = sys.stdin.readline
 
-solve()
+cnt = [[-1] * 1001 for i in range(1001)]
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+n = int(input())
+ans = 0
+
+for i in range(n):
+	x, y = map(int, input().split())
+	hcnt = 0
+	for dir in range(4):
+		nx = x + dx[dir]
+		ny = y + dy[dir]
+		if(nx >= 0 and nx <= 1000 and ny >= 0 and ny <= 1000):
+			ncnt = cnt[nx][ny]
+			if(ncnt >= 0):
+				ncnt += 1
+				hcnt += 1
+				if(ncnt == 3):
+					ans += 1
+				elif(ncnt == 4):
+					ans -= 1
+			cnt[nx][ny] = ncnt
+	if(hcnt == 3): ans += 1
+	cnt[x][y] = hcnt
+	print(ans)	
